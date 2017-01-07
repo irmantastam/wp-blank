60fc3c8f80d4a9b1ad5ee4872f11b51c5c710b91
Irmantas Tamasauskas <irmantastamasauskas@Irmantass-MacBook-Pro.local>
2017-01-07 19:52:33 +0200

    move new files to src folder

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer" role="contentinfo">
-  <p class="footer__copyright copyright">
-    &copy; <?php echo date('Y'); ?> <?php _e('Copyright', 'es6-blank') ?> <?php bloginfo('name'); ?>
-  </p>
-</footer>
-</div>
-<?php wp_footer(); ?>
-</body>
-</html>
