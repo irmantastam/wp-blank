26e47b5d65b74d66695993a015a8146ceb435f22
Irmantas Tamasauskas <irmantastamasauskas@Irmantass-MacBook-Pro.local>
2017-01-07 19:51:13 +0200

    add footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="footer" role="contentinfo">
+  <p class="footer__copyright copyright">
+    &copy; <?php echo date('Y'); ?> <?php _e('Copyright', 'es6-blank') ?> <?php bloginfo('name'); ?>
+  </p>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
