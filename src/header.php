fe03d62098b218414b8f2f983cb55a571fd36f91
Irmantas Tamasauskas <irmantastamasauskas@Irmantass-MacBook-Pro.local>
2017-01-07 19:59:17 +0200

    more identation fix

--- src/header.php
+++ src/header.php
@@ -6,8 +6,8 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    		<meta name="description" content="<?php bloginfo('description'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php bloginfo('description'); ?>">
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
