bc3542e21966a6b80f2f4652b1d7e85fa2b9410a
Irmantas Tamasauskas <irmantastamasauskas@Irmantass-MacBook-Pro.local>
2017-01-07 19:51:43 +0200

    add functions file

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+function es6-blank_nav()
+{
+    wp_nav_menu(
+    array(
+        'theme_location'  => 'header-menu',
+        'menu'            => '',
+        'container'       => 'div',
+        'container_class' => 'menu-{menu slug}-container',
+        'container_id'    => '',
+        'menu_class'      => 'menu',
+        'menu_id'         => '',
+        'echo'            => true,
+        'fallback_cb'     => 'wp_page_menu',
+        'before'          => '',
+        'after'           => '',
+        'link_before'     => '',
+        'link_after'      => '',
+        'items_wrap'      => '<ul>%3$s</ul>',
+        'depth'           => 0,
+        'walker'          => ''
+        )
+    );
+}
+
+?>
