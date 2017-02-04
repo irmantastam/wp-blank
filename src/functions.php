bbfb0255c1319be4bd23eedba71b65a2e94701f9
Irmantas Tamasauskas <irmantastamasauskas@Irmantass-MacBook-Pro.local>
2017-02-04 20:11:40 +0200

    add custom excerpts and register stylesheets

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,6 @@
 <?php
 
-function es6-blank_nav()
-{
+function es6blank_nav() {
     wp_nav_menu(
     array(
         'theme_location'  => 'header-menu',
@@ -23,5 +22,38 @@ function es6-blank_nav()
         )
     );
 }
+
+// Custom Excerpts
+function es6blank_index($length) {
+    return 20;
+}
+
+function es6blank_custom_post($length) {
+    return 40;
+}
+
+// Create the Custom Excerpts callback
+function es6blank_excerpt($length_callback = '', $more_callback = '') {
+    global $post;
+    if (function_exists($length_callback)) {
+        add_filter('excerpt_length', $length_callback);
+    }
+    if (function_exists($more_callback)) {
+        add_filter('excerpt_more', $more_callback);
+    }
+    $output = get_the_excerpt();
+    $output = apply_filters('wptexturize', $output);
+    $output = apply_filters('convert_chars', $output);
+    $output = '<p>' . $output . '</p>';
+    echo $output;
+}
+
+// Load ES6 Blank styles
+function es6blank_styles() {
+  wp_enqueue_style('es6blank', get_template_directory_uri() . '/style.css');
+}
+
+// Add Actions
+add_action('wp_enqueue_scripts', 'es6blank_styles'); // Add bundle CSS
 
 ?>
