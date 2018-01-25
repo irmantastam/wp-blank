e799b39b9f8c9c8b11e573af5bf5f4e69c5bfde9
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2018-01-25 09:17:12 +0200

    allow wordpress to set title tag

--- src/functions.php
+++ src/functions.php
@@ -1,16 +1,20 @@
 <?php
 
-// Add bundle CSS
+/* Specify CSS bundle path */
 function loadStyles() {
   wp_enqueue_style('main', get_template_directory_uri() . '/dist/bundle.css');
 }
 
-// Add bundle JS
+/* Specify JS bundle path */
 function loadScripts() {
   wp_register_script('main', get_template_directory_uri() . '/dist/bundle.js', array('jquery'), '1.0.0');
   wp_enqueue_script('main');
 }
 
+/* Support programmable title tag */
+add_theme_support('title-tag');
+
+/* Register styles and scripts */
 add_action('wp_enqueue_scripts', 'loadStyles');
 add_action('wp_enqueue_scripts', 'loadScripts');
 
