7c95bd2e40d6c925db3d520b56ae06e6728932d6
Irmantas Tamasauskas <irmantas.tam@gmail.com>
2018-08-16 17:24:34 +0300

    update changed bundles path

--- src/functions.php
+++ src/functions.php
@@ -2,12 +2,12 @@
 
 /* Specify CSS bundle path */
 function loadStyles() {
-  wp_enqueue_style('main', get_template_directory_uri() . '/dist/bundle.css');
+  wp_enqueue_style('main', get_template_directory_uri() . '/../dist/bundle.css');
 }
 
 /* Specify JS bundle path */
 function loadScripts() {
-  wp_register_script('main', get_template_directory_uri() . '/dist/bundle.js', array('jquery'), '1.0.0');
+  wp_register_script('main', get_template_directory_uri() . '/../dist/bundle.js', array('jquery'), '1.0.0');
   wp_enqueue_script('main');
 }
 
